bd96377e920f12ec6ea8135c49b3a65b1aaf05f7
JonatanOrtiz <jonataneduard@gmail.com>
2024-08-08 18:29:51 -0300

    Creates SimilarityAppTests file

---
+++ Similarity/SimilarityTests/Features/SimilarityAppTests.swift
@@ -0,0 +1,12 @@
+//
+//  AppEntryPointTests.swift
+//  SimilarityTests
+//
+//  Created by Jonatan Ortiz on 08/08/24.
+//
+
+import XCTest
+import SwiftUI
+@testable import Similarity
+
+final class SimilarityAppTests: XCTestCase {}
